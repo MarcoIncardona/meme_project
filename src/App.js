5ea6ef32732e390bd9d48a79e3182e50fa002475
Marco <incardonamarco0@gmail.com>
2023-05-07 22:04:09 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./style/index.css"
+import { Header } from "./component/Header";
+import { Meme } from "./component/Meme";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export function App(){
+    return(
+        <div className="w-[1000px] h-auto m-auto shadow-xl">       
+            <Header />
+            <Meme />
+        </div>
+    )
 }
-
-export default App;
